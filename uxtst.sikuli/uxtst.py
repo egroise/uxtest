e639e3abfe0c44c1eec59a68677f18211303709c
Eric GROISE <egroise@gmail.com>
2014-05-15 18:39:31 +0200

    Fixes #6 and Fixes #7
    Added DEBUG logs and fixed findFirstFromLeft

--- uxtst.sikuli/uxtst.py
+++ uxtst.sikuli/uxtst.py
@@ -3,7 +3,7 @@
 #| | | |\ \/ / |_   _|___  ___ | |_  ___ 
 #| | | | \  /    | | / _ \/ __|| __|/ __|
 #| |_| | /  \    | ||  __/\__ \| |_ \__ \
-# \___/ /_/\_\   |_| \___||___/ \__||___/  v1.0 - January 2014
+# \___/ /_/\_\   |_| \___||___/ \__||___/  v1.1 - May 2014
 #                                        
 ###############################################################################
 # Author : Eric GROISE
@@ -43,6 +43,7 @@ def startTest(outputPath,options=""):
     global _UXTST_BATCH_
     global _UXTST_OPENREPORT_
     global _UXTST_SPECIALMODE_
+    global _UXTST_DEBUG_
 
     _UXTST_VERSION_ = "1.0"
     _out("UXTEST v"+_UXTST_VERSION_)
@@ -76,7 +77,8 @@ def startTest(outputPath,options=""):
     _UXTST_SESSION_ = "RUNNING"
     _UXTST_BATCH_ = ("BATCHMODE" in options)
     _UXTST_OPENREPORT_ = ("SHOW_REPORT" in options)
-
+    _UXTST_DEBUG_ = ("DEBUG" in options)
+   
     if _UXTST_SPECIALMODE_:
         _UXTST_BATCH_ = True
         _UXTST_OPENREPORT_ = False           
@@ -88,8 +90,13 @@ def startTest(outputPath,options=""):
             file = open(os.path.join(_UXTST_OUTPUT_PATH_,_UXTST_LOG_FILE_),"w")
         else:
             sys.exit()
+
+   
     file.write(str(datetime.now())+" Starting\n")
     file.close()
+    if _UXTST_DEBUG_:
+        _log("DEBUG log enabled")
+    _debug_in("startTest")   
     _new_html("<HTML>")
     _html("<head>")
     _html("<style type='text/css'>thead {font-weight: bold;background-color: #F0F0F0;} html {font-family: Arial;} .error {background-color: red;color: #FFFFFF;} .success {background-color: green;color: #FFFFFF;} .comment {background-color: grey;color: #FFFFFF;} TD {border-style: solid solid;border-width: 1px;border-color: #AAAAAA #EEEEEE #AAAAAA #EEEEEE ;padding-left: 3px;padding-right:15px}table {border-collapse: collapse;}thead {padding-right: 20px;}</style>")
@@ -107,32 +114,33 @@ def endTest():
 # Ends the tests session. (Reports are finalized)
 # The only function you can call after 
 #------------------------------------------------------------------------------
+    _debug_in("endTest")
     from datetime import datetime    
     _isrunning()
     global _UXTST_SESSION_
     _UXTST_SESSION_ = "ENDED"
-    _html("</TABLE><BR/><BR/>")
-    _html("<b>" + str(_UXTST_ASSERT_COUNTER_) + "</b> tests run in <b>" + _seconds(datetime.now() - _UXTST_START_TIME_) + "s</b><BR/>")
-    _xml("<TestCount>" + str(_UXTST_ASSERT_COUNTER_) + "</TestCount>")
-    _xml("<EndTime>" + datetime.now().strftime("%d %m %Y %H:%M:%S") + "</EndTime>")
-    _xml("<TestDurationInSeconds>"+str(int_seconds(datetime.now() - _UXTST_START_TIME_)) + "</TestDurationInSeconds>")    
     if (_UXTST_ASSERT_COUNTER_ != 0):
         frate = 1.0 * _UXTST_ASSERT_FAIL_COUNTER_ / _UXTST_ASSERT_COUNTER_
         srate = 1.0 - frate
     else:
         frate = 0
-        srate = 0
+        srate = 0    
+    _html("</TABLE><BR/><BR/>")
+    _html("<b>" + str(_UXTST_ASSERT_COUNTER_) + "</b> tests run in <b>" + _seconds(datetime.now() - _UXTST_START_TIME_) + "s</b><BR/>")
     _html("<b>" + str(_UXTST_ASSERT_COUNTER_-_UXTST_ASSERT_FAIL_COUNTER_) + "</b> tests succeeded ("+("%.1f" % (srate*100))+"%)<br/>")
     _html("<b>" + str(_UXTST_ASSERT_FAIL_COUNTER_) + "</b> tests failed ("+("%.1f" % (frate*100))+"%)<br/>")
-    _xml("<FailedTestsCount>" + str(_UXTST_ASSERT_FAIL_COUNTER_) + "</FailedTestsCount>")    
-    _xml("<SuccessfulTestsCount>" + str(_UXTST_ASSERT_COUNTER_-_UXTST_ASSERT_FAIL_COUNTER_) + "</SuccessfulTestsCount>")    
     _html("<table><tr>")
-    _xml("</TEST>")    
     if _UXTST_ASSERT_FAIL_COUNTER_ > 0:
         _html("<td width='"+str(1* (300.0 * frate))+"' style='background:red;'>.</td>")
     if _UXTST_ASSERT_COUNTER_-_UXTST_ASSERT_FAIL_COUNTER_ > 0:
         _html("<td width='"+str(1* (300.0 * srate))+"' style='background:green;'>.</td></tr></table>")    
-    _html("</BODY></HTML>")
+    _html("</BODY></HTML>")   
+    _xml("<TestCount>" + str(_UXTST_ASSERT_COUNTER_) + "</TestCount>")
+    _xml("<EndTime>" + datetime.now().strftime("%d %m %Y %H:%M:%S") + "</EndTime>")
+    _xml("<TestDurationInSeconds>"+str(int_seconds(datetime.now() - _UXTST_START_TIME_)) + "</TestDurationInSeconds>")    
+    _xml("<FailedTestsCount>" + str(_UXTST_ASSERT_FAIL_COUNTER_) + "</FailedTestsCount>")    
+    _xml("<SuccessfulTestsCount>" + str(_UXTST_ASSERT_COUNTER_-_UXTST_ASSERT_FAIL_COUNTER_) + "</SuccessfulTestsCount>")    
+    _xml("</TEST>")    
     if _UXTST_OPENREPORT_:
         showTestReport()
       
@@ -142,13 +150,14 @@ def showTestReport():
 # Opens the HTML report in the defauly browser.
 # Must be called after end()
 #------------------------------------------------------------------------------
+    _debug_in("showTestReport")    
     _isended()
     if (_isOnWindows()):
         openApp("explorer.exe " + os.path.join(_UXTST_OUTPUT_PATH_,_UXTST_HTML_FILE_))
     else:
         os.system("open /Applications/Safari.app 'file://" + os.path.join(_UXTST_OUTPUT_PATH_,_UXTST_HTML_FILE_) + "'")
 
-def isVisible(pattern,param1 = "_UNDEFINED_", param2 = "_UNDEFINED_"):
+def assertVisible(pattern,param1 = "_UNDEFINED_", param2 = "_UNDEFINED_"):
     return visible(pattern, param1 , param2)
     
 #------------------------------------------------------------------------------
@@ -162,6 +171,7 @@ def visible(pattern, param1 = "_UNDEFINED_", param2 = "_UNDEFINED_"):
 # If no delay is provided, it will not wait at all (pattern expected at call)
 # Returns True if test is successful (False otherwise)
 #------------------------------------------------------------------------------
+    _debug_in("visible")
     _isrunning()
     delay = 0
     message = ""
@@ -190,14 +200,14 @@ def visible(pattern, param1 = "_UNDEFINED_", param2 = "_UNDEFINED_"):
         _out(_picfind(pattern))
         shutil.copy(_picfind(pattern),os.path.join(_UXTST_OUTPUT_PATH_,"Fail " + str(_UXTST_ASSERT_FAIL_COUNTER_) + " pattern.png"))
         return False  
-
-   
+    
 #------------------------------------------------------------------------------
 def notVisible(pattern, message = ""):
 #------------------------------------------------------------------------------
 # This test is successful if the given pattern is not visible in the screen.
 # Returns True if test is successful (False otherwise)
 #------------------------------------------------------------------------------
+    _debug_in("notVisible")
     _isrunning()
     if (not exists(pattern)):
         _reportTestSuccess("notVisible",message)
@@ -208,13 +218,15 @@ def notVisible(pattern, message = ""):
         return False
 
 #------------------------------------------------------------------------------
-def reportTestFail(message):
+def assertFail(message):
 #------------------------------------------------------------------------------
+    _debug_in("assertFail")
     _reportTestFailure("(custom)",message) 
 
 #------------------------------------------------------------------------------
-def reportTestSucceeded(message):
+def assertSucceeded(message):
 #------------------------------------------------------------------------------
+    _debug_in("assertSucceeded")
     _reportTestSuccess("(custom)",message) 
 
 ###############################################################################   
@@ -225,6 +237,7 @@ def reportTestSucceeded(message):
 def newClick(*args):
 #------------------------------------------------------------------------------
 # Same as the native click() function but supporting timeout. Default timeout is 1s.    
+    _debug_in("newClick")
     nargs = len(args)
     if isinstance(args[nargs-1],int):
         timeout = args[nargs-1]
@@ -242,12 +255,14 @@ def newClick(*args):
 #------------------------------------------------------------------------------
 def wClick(pattern,timeout = 1):
 #------------------------------------------------------------------------------
+    _debug_in("wClick")
     waitingClick(pattern,timeout)
     
 #------------------------------------------------------------------------------
 def waitingClick(pattern,timeout = 1):
 #------------------------------------------------------------------------------
 # Same as the native click() function but supporting timeout. Default timeout is 1s.
+    _debug_in("waitingClick")
     if exists(pattern,timeout):
         click(pattern)
     else:
@@ -257,12 +272,14 @@ def waitingClick(pattern,timeout = 1):
 #------------------------------------------------------------------------------
 def wDoubleClick(pattern,timeout = 1):
 #------------------------------------------------------------------------------
+    _debug_in("wDoubleClick")
     waitingDoubleclick(pattern,timeout)
         
 #------------------------------------------------------------------------------
 def waitingDoubleClick(pattern,timeout = 1):
 #------------------------------------------------------------------------------
 # Same as the native click() function but supporting timeout. Default timeout is 1s.
+    _debug_in("waitingDoubleClick")
     if exists(pattern,timeout):
         doubleClick(pattern)
     else:
@@ -272,11 +289,13 @@ def waitingDoubleClick(pattern,timeout = 1):
 #------------------------------------------------------------------------------
 def oClick(pattern):
 #------------------------------------------------------------------------------
-    optionalClick(pattern)
+   _debug_in("oClick")
+   optionalClick(pattern)
     
 #------------------------------------------------------------------------------
 def optionalClick(pattern):    
 #------------------------------------------------------------------------------
+    _debug_in("optionalClick")
     if (pattern == False):
         return
     if exists(pattern,1):
@@ -285,11 +304,13 @@ def optionalClick(pattern):
 #------------------------------------------------------------------------------
 def oDoubleClick(pattern):
 #------------------------------------------------------------------------------
+    _debug_in("oDoubleClick")
     optionalDoubleClick(pattern)
     
 #------------------------------------------------------------------------------
 def optionalDoubleClick(pattern):
 #------------------------------------------------------------------------------
+    _debug_in("optionalDoubleClick")
     if (pattern == False):
         return
     if exists(pattern,1):
@@ -298,6 +319,7 @@ def optionalDoubleClick(pattern):
 #------------------------------------------------------------------------------
 def takeScreenshot(name = "__NOT DEFINED___"):
 #------------------------------------------------------------------------------
+    _debug_in("takeScreenshot")
     from datetime import datetime
     global _UXTST_SCREENSHOTS_COUNTER_    
     if (name == "__NOT DEFINED___"):
@@ -306,13 +328,13 @@ def takeScreenshot(name = "__NOT DEFINED___"):
     sleep(1)
     _screenshot(name)
     _tr("",None,name,"",name)
-    #_html("<tr><td>"+str(datetime.now())+"</td><td></td><td></td><td>"+name+"</td><td></td><td>"+_htmlimg(name)+"</td><td></td></tr>")
     
 #------------------------------------------------------------------------------
 def findFirstFromLeft(pattern):
 #------------------------------------------------------------------------------
+    _debug_in("findFirstFromLeft")
     try:
-        fs = findAll(text)
+        fs = findAll(pattern)
         sorted_fs = sorted(fs, key=lambda m:m.x)
         return sorted_fs[0]
     except FindFailed:
@@ -321,6 +343,10 @@ def findFirstFromLeft(pattern):
 #------------------------------------------------------------------------------
 def question(title,question): 
 #------------------------------------------------------------------------------
+    _debug_in("question")
+    if _UXTST_BATCH_:
+        _log("BATCHMODE enabled->question skipped and responsed True ("+question+")")
+        return True
     response = javax.swing.JOptionPane.showConfirmDialog(None, question,"UX Tests: " + title, javax.swing.JOptionPane.YES_NO_OPTION,javax.swing.JOptionPane.QUESTION_MESSAGE)
     if response == 0:
         return True
@@ -330,33 +356,41 @@ def question(title,question):
 #------------------------------------------------------------------------------
 def alert(title,message):
 #------------------------------------------------------------------------------
+    _debug_in("alert")
+    if _UXTST_BATCH_:
+        return
     response = javax.swing.JOptionPane.showMessageDialog(None, question,"UX Tests: " + title, javax.swing.JOptionPane.YES_NO_OPTION,javax.swing.JOptionPane.ERROR_MESSAGE)
 
 #------------------------------------------------------------------------------
 def observeWholeScreen():
 #------------------------------------------------------------------------------
+    _debug_in("observeWholeScreen")
     morphTo(Screen())
     
 #------------------------------------------------------------------------------
 def restrictObservationTo(pattern):
 #------------------------------------------------------------------------------
+    _debug_in("restrictObservationTo")
     restrictScan(pattern)
     
 #------------------------------------------------------------------------------
 def restrictScanToWindowContaining(pattern):
 #------------------------------------------------------------------------------
+    _debug_in("restrictScanToWindowContaining")
     click(pattern)
     restrictScanToFocusedWindow()
 
 #------------------------------------------------------------------------------
 def restrictScanToFocusedWindow():
 #------------------------------------------------------------------------------
+    _debug_in("restrictScanToFocusedWindow")
     reg = App.focusedWindow()
     morphTo(reg)
     
 #------------------------------------------------------------------------------
 def openApplication(command):
 #------------------------------------------------------------------------------
+    _debug_in("openApplication")
     if command.find(os.path) == -1:
         command = os.path.join(_getDesktopPath,command)
     openApp(command)
@@ -364,6 +398,7 @@ def openApplication(command):
 #------------------------------------------------------------------------------
 def AppLaunched():
 #------------------------------------------------------------------------------
+    _debug_in("AppLaunched")
     if (os.getenv("UXTST_APP_STARTED","") != ""):
         if os.environ["UXTST_APP_STARTED"] == "YES":
             return True;
@@ -372,6 +407,8 @@ def AppLaunched():
 #------------------------------------------------------------------------------
 def commentTest(comment):
 #------------------------------------------------------------------------------
+    _debug_in("commentTest")
+    _isrunning()    
     _reportComment(comment)
 
 ###############################################################################   
@@ -418,6 +455,13 @@ def _log(text):
     file.write(str(datetime.now())+" "+ text + "\n")
     file.close()
 
+def _debug(text):
+    if _UXTST_DEBUG_:
+        _log("[DEBUG] " + text)
+
+def _debug_in(function):
+    _debug("in> " + function)
+
 def _tr(assertion,status,message,waited,screenshot="",pattern="",isComment=False):
     from datetime import datetime
     if isComment:
@@ -430,9 +474,11 @@ def _tr(assertion,status,message,waited,screenshot="",pattern="",isComment=False
         s = "<TD class='error'>Failure "+str(_UXTST_ASSERT_FAIL_COUNTER_)+"</TD>"
     else:
         s = "<TD></TD>"
-    _html("<TR" + comment + "><TD>" + str(datetime.now()) + "</TD><TD>" + assertion + "</TD>" + s +"<TD>"+ message + "</TD><TD>"+waited+"</TD><TD>"+_htmlimg(screenshot)+"</TD><TD>"+_htmlimg(pattern)+"</TD></TR>")
-    
-
+    if assertion == "(custom)":
+        _html("<TR" + comment + "><TD>" + str(datetime.now()) + "</TD><TD>" + assertion + "</TD>" + s +"<TD>"+ message + "</TD><TD>"+waited+"</TD><TD>"+_htmlimg(screenshot)+"</TD><TD></TD></TR>")
+    else:
+        _html("<TR" + comment + "><TD>" + str(datetime.now()) + "</TD><TD>" + assertion + "</TD>" + s +"<TD>"+ message + "</TD><TD>"+waited+"</TD><TD>"+_htmlimg(screenshot)+"</TD><TD>"+_htmlimg(pattern)+"</TD></TR>")
+  
 def _reportComment(comment):
     _log("# " + comment)
     _tr("",None,comment,"","","",True)
@@ -476,6 +522,9 @@ def _seconds(date):
     s = date.seconds + date.days * 3600.0 * 24 + int(date.microseconds/100000)/10.0
     return str(s) 
 
+def _now():
+    return str(datetime.now())
+
 def int_seconds(date):
     return date.seconds + date.days * 3600 * 24 + int(date.microseconds/10000)
 
@@ -510,7 +559,7 @@ def _isended():
       sys.exit()
 
 def _out(txt):
-    print "UXTST>" + txt
+    print "[UXTST] " + txt
 
 def _isOnWindows():
     return os.sep == "\\"
